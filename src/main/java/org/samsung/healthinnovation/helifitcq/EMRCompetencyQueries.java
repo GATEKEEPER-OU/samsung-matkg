b28b8fcdc3759ab1eef8dd7aba7115780b8061ec
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-06-08 23:32:29 +0200

    Moved ontologies

--- src/main/java/org/samsung/healthinnovation/helifitcq/EMRCompetencyQueries.java
+++ src/main/java/org/samsung/healthinnovation/helifitcq/EMRCompetencyQueries.java
@@ -7,7 +7,6 @@ import tech.oxfordsemantic.jrdfox.client.*;
 import tech.oxfordsemantic.jrdfox.exceptions.JRDFoxException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,7 +19,7 @@ public class EMRCompetencyQueries {
   static final String RESULT_DIR = "output/queries";
 
   static final String DATASTORE_NAME = "GK-Puglia-DataStore";
-  static final String ONTOLOGY_NAME = "HeLiFit-OWL-Functional-Syntax_v1.5.0.owl";
+  static final String ONTOLOGY_NAME = "ontologies/HeLiFit-OWL-Functional-Syntax_v1.5.0.owl";
   static final String QUERIES_PATH = "queries";
   static final String[] QUERY_FILE_EXTS = { "txt" };
 
