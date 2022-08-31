e9c5c81dd5af63a4665db7ddf28ce158657465f8
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-08-31 15:45:09 +0200

    Renamed knowledge construction package to structured knowledge construction

--- src/main/java/org/samsung/healthinnovation/structuredkgconstruction/EMRKGConstruction.java
+++ src/main/java/org/samsung/healthinnovation/structuredkgconstruction/EMRKGConstruction.java
@@ -1,14 +1,13 @@
-package org.samsung.healthinnovation.kgconstruction;
+package org.samsung.healthinnovation.structuredkgconstruction;
 
 import org.apache.commons.io.FileUtils;
 import org.commons.OutputUtils;
 import org.ou.gatekeeper.RDFizer;
+import org.ou.gatekeeper.fhir.adapters.EMRAdapter;
 import org.ou.gatekeeper.fhir.adapters.FHIRAdapter;
-import org.ou.gatekeeper.fhir.adapters.PHRAdapter;
 import org.ou.gatekeeper.rdf.enums.OutputFormat;
 import org.ou.gatekeeper.rdf.mappings.HelifitMapping;
 import org.ou.gatekeeper.rdf.mappings.RMLMapping;
-
 import java.io.File;
 import java.util.Iterator;
 
@@ -16,12 +15,13 @@ import java.util.Iterator;
  * @todo
  * @author Riccardo Pala (riccardo.pala@open.ac.uk)
  * */
-public class PHRKGConstruction {
+public class EMRKGConstruction {
 
-  static final String DATASETS_DIR = "datasets/data-phr";
-  static final String OUTPUT_DIR = "output/kg-phr";
+  static final String DATASETS_DIR = "datasets/data-emr";
+  static final String OUTPUT_DIR = "output/kg-emr";
 
   public static void main(String[] args) {
+    // @todo NoSuchFileException: datasets/data-emr dataset missing
     File datasetsDir = new File(DATASETS_DIR);
     File outputDir = new File(OUTPUT_DIR);
     outputDir.mkdir();
@@ -31,7 +31,7 @@ public class PHRKGConstruction {
     String[] exts = {"json"};
     Iterator<File> datasets = FileUtils.iterateFiles(datasetsDir, exts, false);
 
-    FHIRAdapter converter = PHRAdapter.create();
+    FHIRAdapter converter = EMRAdapter.create();
     RMLMapping mapping = HelifitMapping.create(OutputFormat.NTRIPLES);
 
     RDFizer.trasform(datasets, converter, mapping, outputDir, outputExt);
--- src/main/java/org/samsung/healthinnovation/structuredkgconstruction/PHRKGConstruction.java
+++ src/main/java/org/samsung/healthinnovation/structuredkgconstruction/PHRKGConstruction.java
@@ -1,13 +1,14 @@
-package org.samsung.healthinnovation.kgconstruction;
+package org.samsung.healthinnovation.structuredkgconstruction;
 
 import org.apache.commons.io.FileUtils;
 import org.commons.OutputUtils;
 import org.ou.gatekeeper.RDFizer;
-import org.ou.gatekeeper.fhir.adapters.EMRAdapter;
 import org.ou.gatekeeper.fhir.adapters.FHIRAdapter;
+import org.ou.gatekeeper.fhir.adapters.PHRAdapter;
 import org.ou.gatekeeper.rdf.enums.OutputFormat;
 import org.ou.gatekeeper.rdf.mappings.HelifitMapping;
 import org.ou.gatekeeper.rdf.mappings.RMLMapping;
+
 import java.io.File;
 import java.util.Iterator;
 
@@ -15,12 +16,13 @@ import java.util.Iterator;
  * @todo
  * @author Riccardo Pala (riccardo.pala@open.ac.uk)
  * */
-public class EMRKGConstruction {
+public class PHRKGConstruction {
 
-  static final String DATASETS_DIR = "datasets/data-emr";
-  static final String OUTPUT_DIR = "output/kg-emr";
+  static final String DATASETS_DIR = "datasets/data-phr";
+  static final String OUTPUT_DIR = "output/kg-phr";
 
   public static void main(String[] args) {
+    // @todo NoSuchFileException: datasets/data-phr dataset missing
     File datasetsDir = new File(DATASETS_DIR);
     File outputDir = new File(OUTPUT_DIR);
     outputDir.mkdir();
@@ -30,7 +32,7 @@ public class EMRKGConstruction {
     String[] exts = {"json"};
     Iterator<File> datasets = FileUtils.iterateFiles(datasetsDir, exts, false);
 
-    FHIRAdapter converter = EMRAdapter.create();
+    FHIRAdapter converter = PHRAdapter.create();
     RMLMapping mapping = HelifitMapping.create(OutputFormat.NTRIPLES);
 
     RDFizer.trasform(datasets, converter, mapping, outputDir, outputExt);
