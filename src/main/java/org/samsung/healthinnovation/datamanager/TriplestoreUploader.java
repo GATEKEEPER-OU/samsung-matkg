04d947e87f7343ec305bb412b29d06e039ce8aa3
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-08-31 15:43:54 +0200

    Added empty Blazegraph management class

---
+++ src/main/java/org/samsung/healthinnovation/datamanager/TriplestoreUploader.java
@@ -0,0 +1,13 @@
+package org.samsung.healthinnovation.datamanager;
+
+/**
+ * @todo
+ * @author Riccardo Pala (riccardo.pala@open.ac.uk)
+ * */
+public class TriplestoreUploader {
+
+  public static void main(String[] args) {
+
+  }
+
+}
