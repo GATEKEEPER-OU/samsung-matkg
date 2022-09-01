eb53e5a4dc97b4cbab8d0ff7fc79cdbe8cb76e8e
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-09-01 18:10:17 +0200

    Added RestUtils class

---
+++ src/main/java/org/commons/RestUtils.java
@@ -0,0 +1,46 @@
+package org.commons;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @todo
+ * @author Riccardo Pala (riccardo.pala@open.ac.uk)
+ * */
+public class RestUtils {
+
+  /**
+   * @param httpClient
+   * @param postUrl
+   * @param content
+   * @throws IOException
+   */
+  public static void makePost(CloseableHttpClient httpClient, String postUrl, File content) throws IOException {
+    // Set request headers
+    InputStreamEntity reqEntity = new InputStreamEntity(
+        new FileInputStream(content),
+        -1,
+        ContentType.TEXT_PLAIN
+    );
+    reqEntity.setChunked(true);
+
+    // Init post request
+    HttpPost httpPost = new HttpPost(postUrl);
+    httpPost.setEntity(reqEntity);
+
+//    System.out.println("Executing request: " + httpPost.getRequestLine()); // DEBUG
+    CloseableHttpResponse response = httpClient.execute(httpPost);
+
+//    System.out.println(response.getStatusLine()); // DEBUG
+//    System.out.println(EntityUtils.toString(response.getEntity())); // DEBUG
+    response.close();
+  }
+
+}
